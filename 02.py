1096a52f8a6915f48eab809558eeea6ce2cf6331
Nivedita <niveditan.ml25@sbjit.edu.in>
2026-08-07 15:51:15 +0530

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,13 @@
+#2. To write a Python program to interchange the values of two variables.
+a = 5
+b = 10
+
+print(f"Before swapping: a = {a}, b = {b}")
+
+# Using a temporary variable
+temp = a
+a = b
+b 
+= temp
+
+print(f"After swapping (temp variable): a = {a}, b = {b}")
