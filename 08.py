237171b304fe745aae23d5d0a26efa0023972cdd
Nivedita <niveditan.ml25@sbjit.edu.in>
2026-08-10 12:03:23 +0530

    Python 08

--- 08.py
+++ 08.py
@@ -2,7 +2,6 @@
 # number.
 num = int(input("Enter a number: "))
 limit = int(input("Enter the limit for the multiplication table (e.g., 10): "))
-
 print(f"\nMultiplication Table for {num} up to {limit}:")
 for i in range(1, limit + 1):
     print(f"{num} x {i} = {num * i}")
